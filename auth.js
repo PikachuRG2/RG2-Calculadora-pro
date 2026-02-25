1538afdeca044442bdeefec92d37bbbca7aa0489
Rodrigo <rafael2019rg@gmail.com>
2026-02-25 01:58:37 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   measurementId: 'G-8X75M0H60X'
 }
 
-const POST_LOGIN_URL = 'https://rg2tvfree.blogspot.com/p/tv-free.html'
+const POST_LOGIN_URL = 'https://rg-2-cal-2026.vercel.app/'
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
